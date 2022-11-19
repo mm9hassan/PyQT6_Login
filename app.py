592a4c0fbde9adf1d8ba3423d3a50d1251298662
Muhammad Hassan <mm9hassan@gmail.com>
2022-11-19 21:12:32 +0500

    login done

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from PyQt6.QtWidgets import QApplication,QDialog,QStackedWidget
+from PyQt6.QtWidgets import QApplication,QDialog,QStackedWidget,QMessageBox
 from PyQt6.uic import load_ui
 import sys
+import sqlite3
+db=sqlite3.connect('system.db')
+command=db.cursor()
 
 
 
@@ -8,40 +11,69 @@ import sys
 class Wellcome(QDialog):
     def __init__(self) -> None:
         super(Wellcome,self).__init__()
-        load_ui.loadUi('wellcome.ui',self)
-        widget.setFixedWidth(400)
-        widget.setFixedHeight(250)
-        self.btn_login.clicked.connect(self.login_p)
-        self.btn_create.clicked.connect(self.create_p)
-
-
-    def login_p(self):
-        login=Login()
-        widget.addWidget(login)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-    def create_p(self):
-        create=Create()
-        widget.addWidget(create)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-
-class Login(QDialog):
-    def __init__(self) -> None:
-        super(Login,self).__init__()
         load_ui.loadUi('login.ui',self)
-
         widget.setFixedWidth(400)
         widget.setFixedHeight(300)
+        self.btn_enter.clicked.connect(self.login)
+        
+    # login option
+    
+    def login(self):
+        user=self.id.text()
+        password=self.password.text()
+        command.execute('SELECT * FROM login')
+        back=command.fetchall()
+        if len(user)==0 or len(password)==0:
+            print('fill value')
+
+        elif user== back[0][1] and  password==back[0][2]:
+            print('working')
+  
+            menu= Menu()
+            widget.addWidget(menu)
+            widget.setCurrentIndex(widget.currentIndex()+1)
+        else:
+            print ('error')
+        
+        
+        
+        
+        
+
+        # -----Class Menu-------
+
+        # _____END_____
 
 
-
-class Create(QDialog):
+class Menu(QDialog):
     def __init__(self) -> None:
-        super(Create,self).__init__()
-        load_ui.loadUi('create.ui',self)
-        widget.setFixedWidth(400)
-        widget.setFixedHeight(300)
+        super(Menu,self).__init__()
+        load_ui.loadUi('menu.ui',self)
+        
+        widget.setMinimumWidth(700)
+        widget.setMinimumHeight(500)
+
+
+        self.save.clicked.connect(self.option)
+    
+
+    def option(self):
+        name=self.name.text()
+        father=self.father.text()
+        cnic=self.cnic.text()
+        cell=self.cell.text()
+        if len(name)==0 or len(father)==0 or len(cnic)==0 or len(cell)==0:
+            print('fill all value')
+        else:
+            command.execute('INSERT INTO profile (name,f_name,cnic,cell) VALUES ("{}","{}","{}","{}")'.format(name,father,cnic,cell))
+            db.commit()
+            db.close()
+
+            print('working')
+
+    
+
+    
 
         
 
